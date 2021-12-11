e3d439d11f36eced51ecd134231b2e9c45c5283a
UnrelatedString <unrelatedstring@gmail.com>
2021-12-11 00:10:36 -0500

    refreshing

--- aoc11-2.py
+++ aoc11-2.py
@@ -8,15 +8,16 @@ os = {}
 for y,r in enumerate(ls):
     for x,n in enumerate(r):
         os[complex(x,y)] = int(n)
-fs = 0
-for _ in range(100):
+t = 0
+while(True):
+    t += 1
     for o in os:
         os[o] += 1
     fd = set()
     while True:
         for o in os:
             if os[o] > 9 and o not in fd:
-                fs += 1
+                
                 fd.add(o)
                 for s in 1,-1,1j,-1j,1+1j,1-1j,-1+1j,-1-1j:
                     if o+s in os:
@@ -27,5 +28,7 @@ for _ in range(100):
     for o in os:
         if os[o] > 9:
             os[o] = 0
+    if fd == set(os.keys()):
+        break
 
-print(fs)
+print(t)

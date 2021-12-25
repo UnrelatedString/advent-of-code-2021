e56a22f2c9d98b39285df3e3965c074352f3ed81
UnrelatedString <33167175+UnrelatedString@users.noreply.github.com>
2021-12-25 00:26:10 -0500

    that was a

--- aoc25.py
+++ aoc25.py
@@ -1,4 +1,45 @@
-import sys
-from stuff import *
+ls = [l for l in iter(input,'')]
 
-ls = lines()#chunks()#nums()
+g = {}
+w,h = len(ls[0]),len(ls)
+for y,r in enumerate(ls):
+    for x,v in enumerate(r):
+        if v != '.':
+            g[complex(x,y)] = v
+
+t = 0
+while True:
+    t += 1
+    moved = False
+    ng = {}
+    for z,v in g.items():
+        x,y = z.real,z.imag
+        if v == '>':
+            nz = complex((x+1)%w,y)
+            if nz in g:
+                ng[z] = v
+            else:
+                ng[nz] = v
+                moved = True
+                #print(f'moved {z} {nz}')
+        else:
+            ng[z] = v
+    g = ng
+    ng = {}
+    for z,v in g.items():
+        x,y = z.real,z.imag
+        if v == 'v':
+            nz = complex(x,(y+1)%h)
+            if nz in g:
+                ng[z] = v
+            else:
+                ng[nz] = v
+                moved = True
+                #print(f'moved {z} {nz}')
+        else:
+            ng[z] = v
+    g = ng
+    if not moved:
+        break
+
+print(t)
